18a7e0994d9205c950ecc06d2abd460084922eff
Allen Welch <allenjwelch@gmail.com>
2018-06-13 17:37:40 -0400

    scrape working successfully

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require("express");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // Require request and cheerio. This makes the scraping possible
 const request = require("request");
@@ -21,7 +21,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// ==== Routes ====
+app.get('/scrape', function(req, res) {
+  request("https://www.nytimes.com/section/us", function(error, response, html) {
+    let $ = cheerio.load(html);
+    let result = {};
+    $("div.story-body").each(function(i, element) {
+      
+      result.title = $(element).children().children().children("h2.headline").text().trim();
+      result.summary = $(element).children().children().children("p.summary").text();
+      result.link = $(element).children().attr("href");
+      
+      // results.push({
+      //   title: title,
+      //   summary: summary,
+      //   link: link,
+      // });
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+      .then(function(dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        return res.json(err);
+      });
+    });
+
+    // console.log('===> US Headlines from https://www.nytimes.com/section/us <===');
+    // console.log(result);
+
+    res.send("Scrape Complete");
+
+  })
+});
 
 
 // Start the server
